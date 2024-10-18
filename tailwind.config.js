1a909fc0bb279913baab87effccf902abe3dc1a9
Sankalp kumar <sankalpkumar911@gmail.com>
2024-10-19 04:51:20 +0530

    Clone of AllBirds using Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens:{
+      'mobile':{'max': '500px'}
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
